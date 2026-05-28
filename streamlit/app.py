395e503dc5a386ddfa3c13105f8f5fe419afe81b
Bruno-115 <Bruno-115@outlook.com>
2026-05-27 23:47:48 -0300

    Teste

--- streamlit/app.py
+++ streamlit/app.py
@@ -68,9 +68,9 @@ Gender = st.radio("Escolha o genêro da pessoa ", options=list(opcoes_genero.key
 
 Age = st.slider("Qual a idade da pessoa?", 0, 110, 0)
 
-Height = st.number_input("Qual a altura da pessoa?",max_value=2.3)
+Height = st.number_input("Qual a altura da pessoa? Max:2.3M",max_value=2.3)
 
-Weight = st.number_input("Qual o peso da pessoa em KG?",max_value=400)
+Weight = st.number_input("Qual o peso da pessoa em KG? Max:400KG",max_value=400)
 
 BMI = Weight / (Height ** 2) if Height > 0 else 0
 
@@ -164,9 +164,9 @@ if st.button("Verificar"):
     try:
 
         response = requests.post(
-            "http://api:5000/predict",
+            "https://techchallenger4.onrender.com/predict",
             json=input_data,
-            timeout=10
+            timeout=30
         )
 
         if response.status_code == 200:
