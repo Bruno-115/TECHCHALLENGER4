fd3359ad4a880457fb894abea7499bda8f69574c
Bruno-115 <Bruno-115@outlook.com>
2026-05-28 20:26:26 -0300

    TESTE

--- streamlit/app.py
+++ streamlit/app.py
@@ -68,7 +68,7 @@ Gender = st.radio("Escolha o genêro da pessoa ", options=list(opcoes_genero.key
 
 Age = st.slider("Qual a idade da pessoa?", 0, 110, 0)
 
-Height = st.number_input("Qual a altura da pessoa? Max:2.3M",max_value=2.3)
+Height = st.number_input("Qual a altura da pessoa? Max:2.3MMMM",max_value=2.3)
 
 Weight = st.number_input("Qual o peso da pessoa em KG? Max:400KG",max_value=400)
 
