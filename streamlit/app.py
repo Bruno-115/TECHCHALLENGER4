6ee7240a13c5a19c1ce2d5d680e97005526087b5
Bruno-115 <Bruno-115@outlook.com>
2026-05-28 21:27:45 -0300

    teste

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Tech Challenge",
+    layout="wide"
+)
 
 st.title("Tech Challenge")
-st.write("Aplicação funcionando")
 
-st.sidebar.success("Teste")
+st.page_link("pages/1_dashboard.py", label="Dashboard")
+st.page_link("pages/2_modeloML.py", label="Modelo ML")
