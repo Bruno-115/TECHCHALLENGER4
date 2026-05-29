4f45ad9fc083580f00047389e4df9f126c4d34e7
Bruno-115 <Bruno-115@outlook.com>
2026-05-28 22:45:06 -0300

    fix height and weight, min = 1

--- streamlit/pages/2_modeloML.py
+++ streamlit/pages/2_modeloML.py
@@ -68,9 +68,9 @@ Gender = st.radio("Escolha o genêro da pessoa ", options=list(opcoes_genero.key
 
 Age = st.slider("Qual a idade da pessoa?", 0, 110, 0)
 
-Height = st.number_input("Qual a altura da pessoa? Max:2.3M",max_value=2.3, min_value=1)
+Height = st.number_input("Qual a altura da pessoa? Max:2.3M", 1,2.3,1)
 
-Weight = st.number_input("Qual o peso da pessoa em KG? Max:400KG", min_value=1,max_value=400)
+Weight = st.number_input("Qual o peso da pessoa em KG? Max:400KG", 1,400,1)
 
 BMI = Weight / (Height ** 2) if Height > 0 else 0
 
